38ae9c36a9bb8ed36a6d4945d8b1ffe894b51e26
marcos tulyo oliveira martins <mtom@academico.ufpb.br>
2026-05-14 13:09:11 -0300

    mapeamento.hpp with new structures (graphic and mapping)

--- mapeamento.hpp
+++ mapeamento.hpp
@@ -1,13 +1,19 @@
 #pragma once
 
 #include <iostream>
-#include <math.h>
 #include <vector>
+#include <cmath>
 
 typedef struct {
     float x;
     float y;
 } Ponto;
+
+typedef struct { 
+float x; //coordenada x 
+float y; // coordenada y
+int contagem; // quantas vezes o drone detectou essa base  para calcular o centro
+} base ;
 
 class Mapeamento {
     private:
@@ -16,10 +22,18 @@ class Mapeamento {
         std::vector<Ponto> Pontos;
         std::vector<float> Distancia;
         std::vector<float> Media;
+
+// para o mapeamneto e gráfico
+        std::vector<Ponto> pontosBrutos; // grafico de todas as coordenadas 
+        std::<Base> mapaFinal; // gráfico de coordenadas válidas 
+
     public:
-        Mapeamento(){x = 0; y = 0; dx = 0; dy = 0; qtd = 0; Pontos = {}; Distancia = {}; Media = {};}
+        Mapeamento(){x = 0; y = 0; dx = 0; dy = 0; qtd = 0; Pontos = {}; Distancia = {}; Media = {}; pontosBrutos = {}; mapaFinal =;
         
         void mapearCoordenadas();
         void calcularDistancia();
         void calcularMedia();
+
+        void processarPonto(float px, float py); // escolhe se é base existente ou nova 
+        void gerarArquivoPlot();  // gera arquivos .txt para cumprir a visualização              
 };
