a7d3415278918f2eaf21e420a4c4b6624f706c4e
marcos tulyo oliveira martins <mtom@academico.ufpb.br>
2026-05-25 03:46:56 -0300

    ATUALIZEI

--- mapeamento.hpp
+++ mapeamento.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #include <vector>
 
 typedef struct {
@@ -9,17 +9,28 @@ typedef struct {
     float y;
 } Ponto;
 
+typedef struct {
+ float x;
+ float y;
+ int contagem;
+}base ;
+
 class Mapeamento {
     private:
-        float x, y, dx, dy;
+ std::vector<Ponto> pontosBrutos; 
+ std::vector<Base> mapaFinal;     
+
+        float tempX, tempY;
         int qtd;
-        std::vector<Ponto> Pontos;
-        std::vector<float> Distancia;
-        std::vector<float> Media;
-    public:
-        Mapeamento(){x = 0; y = 0; dx = 0; dy = 0; qtd = 0; Pontos = {}; Distancia = {}; Media = {};}
+
+        Mapeamento() : qtd(0), tempX(0), tempY(0) {
+            pontosBrutos = {};
+            mapaFinal = {};
+        }
         
-        void mapearCoordenadas();
-        void calcularDistancia();
-        void calcularMedia();
+       
+ void mapearCoordenadas();
+
+        void processarPonto(float px, float py);
+        void gerarArquivoPlot();
 };
